6554a6cda50d60cc83151c30727e6bc7cbdacfe1
Leon Forde <leonforde1201@gmail.com>
2026-04-17 13:01:10 +0100

    Initialize diagnostics module with exports

---
+++ src/diagnostics/__init__.py
@@ -0,0 +1,10 @@
+from .stylized_facts import compute_benchmark
+from .tests import run_test_suite, print_test_suite
+from .demo import run_cvae_demo
+
+__all__ = [
+    "compute_benchmark",
+    "run_test_suite",
+    "print_test_suite",
+    "run_cvae_demo",
+]
